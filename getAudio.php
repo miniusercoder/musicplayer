5c6cadf7d9c2f189ae20996f320048a87d8cb3b9
MiniUser <HopeShare1>
2020-03-04 23:23:57 +0300

    Init commit

---
+++ getAudio.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+if (isset($_GET['seed']))
+    $seed = (int)$_GET['seed'];
+else
+    $seed = time();
+$array = scandir(__DIR__ . "/playlist");
+$return = ["audio" => [], "offset" => 0];
+unset($array[0]);
+unset($array[1]);
+seededShuffle($array, $seed);
+if (isset($_GET['offset'])) {
+    $offset = $_GET['offset'];
+    if ($offset < 0 or $offset >= count($array))
+        die("error");
+} else
+    $offset = 0;
+$getID3 = new getID3;
+
+$tags = $getID3->analyze("playlist/{$array[$offset]}");
+$return['audio'] = [
+    "track" => $array[$offset],
+    "tags" => $tags['tags']['id3v2']
+];
+$return['offset'] = $offset;
+echo json_encode($return);
+
+function seededShuffle(array &$array, int $seed)
+{
+    mt_srand($seed);
+    sort($array);
+    for ($i = 0; $i < count($array); $i++) {
+        $rand = mt_rand(0, count($array) - 1);
+        $temp = $array[$rand];
+        $array[$rand] = $array[$i];
+        $array[$i] = $temp;
+    }
+    return $array;
+}
---
+++ index.js
@@ -0,0 +1,98 @@
+let offset = 0;
+let seed = Math.floor(Math.random() * 100000);
+let audio_tag = document.getElementById("audio");
+let pause = document.getElementById("pause");
+let download = document.getElementById("download");
+let seed_input = document.getElementById("seed");
+let track_number = document.getElementById("track_number");
+seed_input.value = seed;
+track_number.value = offset;
+
+function getAudio() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "getAudio.php?offset=" + offset + "&seed=" + seed, true);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            let response = xhr.responseText;
+            console.log(response);
+            if (response === "error") {
+                offset = 0;
+                seed = Math.floor(Math.random() * 100000);
+                seed_input.value = seed;
+                getAudio();
+                return 0;
+            }
+            response = JSON.parse(xhr.responseText);
+            let audio = response.audio;
+            track_number.value = offset + 1;
+            offset = response.offset;
+            audio_tag.src = "/playlist/" + audio.track;
+            document.title = audio.tags.artist[0] + " - " + audio.tags.title[0];
+            document.getElementById("author").innerText = audio.tags.artist[0];
+            document.getElementById("name").innerText = audio.tags.title[0];
+            document.getElementById("fullname").innerText = audio.tags.artist[0] + " - " + audio.tags.title[0];
+            document.getElementById("album").innerText = audio.tags.album[0];
+            audio_tag.onended = function () {
+                offset = +offset + 1;
+                getAudio()
+            };
+            audio_tag.oncanplay = function () {
+                audio_tag.play();
+            };
+            console.log(audio);
+        }
+    }
+}
+
+getAudio();
+
+
+document.getElementById("next").onclick = function () {
+    offset = +offset + 1;
+    getAudio();
+};
+document.getElementById("previous").onclick = function () {
+    if (offset - 1 >= 0) {
+        offset = +offset - 1;
+        getAudio();
+    }
+};
+pause.onclick = function () {
+    if (audio_tag.paused === true)
+        audio_tag.play();
+    else
+        audio_tag.pause();
+};
+audio_tag.onplay = function () {
+    pause.innerText = "Pause";
+};
+audio_tag.onpause = function () {
+    pause.innerText = "Play";
+};
+download.onclick = function () {
+    let link = document.createElement("a");
+    link.href = audio_tag.src;
+    link.download = document.getElementById("fullname").innerText + ".mp3";
+    link.click();
+};
+seed_input.onchange = function () {
+    seed = seed_input.value;
+    getAudio();
+};
+track_number.onchange = function () {
+    if (track_number.value <= 0)
+        track_number.value = 1;
+    if (track_number.value > 195)
+        track_number.value = 1;
+    offset = track_number.value - 1;
+    getAudio();
+};
+document.onkeydown = function (event) {
+    if (event.code === "Space") {
+        if (audio_tag.paused === true)
+            audio_tag.play();
+        else
+            audio_tag.pause();
+    }
+};
