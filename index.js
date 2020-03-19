bf0f49d1610b55e873df976184073408dbe48190
miniusercoder <admin@miniuser.ru>
2020-03-19 23:16:17 +0300

    Volume changing added.

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ let pause = document.getElementById("pause");
 let download = document.getElementById("download");
 let seed_input = document.getElementById("seed");
 let track_number = document.getElementById("track_number");
-let point = document.getElementById("point");
+let pointTime = document.getElementById("pointTime");
 let current = document.getElementById("currentTime");
 let line = document.getElementById("line");
 let duration = document.getElementById("duration");
+let volume = document.getElementById("volume");
+let pointVolume = document.getElementById("pointVolume");
 seed_input.value = seed;
 track_number.value = offset;
 
@@ -34,16 +36,16 @@ function getAudio() {
             offset = response.offset;
             track_number.value = +offset + 1;
             audio_tag.src = "playlist/" + audio.track;
-            document.title = audio.tags.artist[0] + " - " + audio.tags.title[0];
-            document.getElementById("author").innerText = audio.tags.artist[0];
-            document.getElementById("name").innerText = audio.tags.title[0];
-            document.getElementById("fullname").innerText = audio.tags.artist[0] + " - " + audio.tags.title[0];
-            document.getElementById("album").innerText = audio.tags.album[0];
             audio_tag.onended = function () {
                 offset = +offset + 1;
                 getAudio()
             };
             audio_tag.oncanplay = function () {
+                document.title = audio.tags.artist[0] + " - " + audio.tags.title[0];
+                document.getElementById("author").innerText = audio.tags.artist[0];
+                document.getElementById("name").innerText = audio.tags.title[0];
+                document.getElementById("fullname").innerText = audio.tags.artist[0] + " - " + audio.tags.title[0];
+                document.getElementById("album").innerText = audio.tags.album[0];
                 duration.innerText = Math.floor(audio_tag.duration / 60) +
                     ":" + addZeroes((Math.floor(audio_tag.duration - ((Math.floor(audio_tag.duration / 60)) * 60))).toString(), 2);
                 audio_tag.play();
@@ -77,6 +79,11 @@ audio_tag.onplay = function () {
 };
 audio_tag.onpause = function () {
     pause.innerText = "Play";
+};
+audio_tag.onvolumechange = function () {
+    pointVolume.style.transform = "translateX(17px) translateY(" +
+        (((1 - audio_tag.volume) * (volume.offsetHeight)) - 1.7)
+        + "px)";
 };
 download.onclick = function () {
     let link = document.createElement("a");
@@ -116,14 +123,18 @@ document.onkeydown = function (event) {
 };
 
 line.onclick = function (e) {
-    let percent = (+e.pageX - +line.getBoundingClientRect().x) * 100 / line.offsetWidth;
+    let percent = (+e.clientX - +line.getBoundingClientRect().x) * 100 / line.offsetWidth;
     audio_tag.currentTime = (audio_tag.duration / 100 * percent);
+};
+
+volume.onclick = function (e) {
+    audio_tag.volume = parseFloat((volume.getBoundingClientRect().bottom - e.clientY - 1.7) / volume.offsetHeight);
 };
 
 setInterval(function () {
     current.innerText = Math.floor(audio_tag.currentTime / 60) +
         ":" + addZeroes((Math.floor(audio_tag.currentTime - ((Math.floor(audio_tag.currentTime / 60)) * 60))).toString(), 2);
-    point.style.transform = "translateY(-7px) translateX(" + (line.offsetWidth - 4) / 100 * (audio_tag.currentTime * 100 / audio_tag.duration) + "px)";
+    pointTime.style.transform = "translateY(-7px) translateX(" + (line.offsetWidth - 4) / 100 * (audio_tag.currentTime * 100 / audio_tag.duration) + "px)";
 }, 100);
 
 function addZeroes(str, lenght) {
